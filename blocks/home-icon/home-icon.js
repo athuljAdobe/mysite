e53174d8c7bf032106ea391cebe8bfdf76e8270e
Athul Ganesh <athulgnsh55@gmail.com>
2026-03-02 10:28:00 +0530

    added an icon

---
+++ blocks/home-icon/home-icon.js
@@ -0,0 +1,9 @@
+export default function decorate(block) {
+  const iconName = block.textContent.trim();
+
+  block.innerHTML = `
+    <span class="icon icon-${iconName}">
+      <img src="/icons/${iconName}.svg" alt="${iconName}" />
+    </span>
+  `;
+}
